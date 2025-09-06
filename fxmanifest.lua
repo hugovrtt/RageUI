6221ab53cd15ac4259d4ff1e14f6e037fabd5d82
hugov <hugovrt1@gmail.com>
2025-09-06 03:42:12 +0200

    feat(rageui) add menu

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "cerulean"
+game "gta5"
+author "hugo"
+description "RageUI Menu"
+lua54 "yes"
+
+client_scripts {
+    "RageUI/RageUI.lua",
+    "RageUI/Menu.lua",
+    "RageUI/MenuController.lua",
+    "RageUI/components/*.lua",
+    "RageUI/elements/*.lua",
+    "RageUI/items/*.lua",
+}
